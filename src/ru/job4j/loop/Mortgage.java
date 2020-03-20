9d2f1410a94ee95fefb6b2b10743e50ed62c8d93
Haliz <foril2n1@rambler.ru>
2020-03-20 21:46:00 +0300

    5.7. Ипотека.[#248099]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        double fullAmount = (amount + (amount * percent/100));
+        int year = 1;
+        while (salary < fullAmount ) {
+            year ++;
+            fullAmount = fullAmount - salary;
+        }
+        return year;
+    }
+}
