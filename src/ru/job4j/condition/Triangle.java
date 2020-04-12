514ddfacdbd21eaaef169411364b2d426c098ecb
Haliz <foril2n1@rambler.ru>
2020-04-12 18:20:22 +0300

    2.2. Рефакторинг - Площадь треугольника.[#267877]

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -1,7 +1,46 @@
 package ru.job4j.condition;
 
 public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double period(double a, double b, double c) {
+       return   a/2+b/2+c/2;
+
+    }
+
+    public double area() {
+        double rsl = -1;
+        double sideA = first.distance(second);
+        double sideB = first.distance(third);
+        double sideC = second.distance(third);
+        double p = period(sideA, sideB, sideC);
+        if (exist(sideA, sideB, sideC)) {
+            rsl = Math.sqrt(p*(p-sideA)*(p-sideB)*(p-sideC));
+        }
+        /** Так думаю было бы короче воспользоваться методом который был давно сделан
+         * rsl = TrgArea.area(sideA, sideB, sideC)
+         * Можно было бы удалить здесь метод и переменную связанную с полупериметром (там он есть).
+         */
+        return rsl;
+    }
+
     public static boolean exist(double ab, double ac, double bc) {
         return ab + ac > bc && ac + bc > ab && ab + bc > ac;
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 5);
+        Point c = new Point(10, 0);
+        Triangle triangle = new Triangle(a,b,c);
+        System.out.println("Площадь треукольника" + triangle.area());
     }
 }
--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -15,5 +16,24 @@ public class TriangleTest {
     public void whenNoExist() {
         boolean result = Triangle.exist(2.0, 2.0, 0.0);
         assertThat(result, is(false));
+    }
+
+    @Test
+    public void AreaTrue() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 5);
+        Point c = new Point(10, 0);
+        Triangle triangle = new Triangle(a,b,c);
+        double expected = 25;
+        Assert.assertEquals(expected, triangle.area(), 0.01);
+    }
+    @Test
+    public void AreaFalse() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 0);
+        Point c = new Point(10, 0);
+        Triangle triangle = new Triangle(a,b,c);
+        double expected = -1;
+        Assert.assertEquals(expected, triangle.area(), 0.01);
     }
 }
